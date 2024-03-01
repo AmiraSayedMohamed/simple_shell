cc03f587956d179c305e910c2bfd0458de4027b0
AmiraSayedMohamed <amira.sayedza@gmail.com>
2024-03-01 13:23:45 -0800

    edit main.h

--- main.h
+++ main.h
@@ -141,12 +141,103 @@ static char **allocateMemory(int numWords);
 static void exWords(const char *input, const char *delimiter, char **result);
 char **splitString(const char *input, const char *delimiter);
 
-/* realloc_memory.c*/
+/* reallocateMemory.c*/
 void *reallocateMemory(void *ptr, unsigned int prevSize, unsigned int newSize);
 void freeStringArray(char **array);
 void *fillMemory(void *ptr, unsigned char byte, unsigned int numBytes);
 int freeAndNull(void **ptr);
 
+/*string_list.c*/
+list_t *add_node_start(list_t **head, const char *str, int num);
+list_t *add_node_end(list_t **head, const char *str, int num);
+size_t print_str_list(const list_t *h);
+int delete_node_at_index(list_t **head, unsigned int index);
+void free_str_list(list_t **head_ptr);
+
+/* shell_util1.c  */
+int _myhistory(info_t *info);
+int unset_alias(info_t *info, char *str);
+int set_alias(info_t *info, char *str);
+int print_alias(list_t *node);
+int _myalias(info_t *info);
+
+/* shell_util.c */
+int _exit_shell(info_t *info);
+int _change_directory(info_t *info);
+int _display_help(info_t *info);
+
+/* shell_history.c */
+char *get_history_file(info_t *info);
+int write_history(info_t *info);
+static char *read_history_from_file(info_t *info, int fd);
+int read_history(info_t *info);
+
+/* shell_exec.c */
+int run_shell(info_t *info, char **av);
+int find_builtin(info_t *info);
+void find_command(info_t *info);
+void fork_command(info_t *info);
+
+/* list_functions.c */
+size_t list_length(const list_t *head);
+char **list_to_strings(list_t *head);
+size_t print_list_nodes(const list_t *head);
+list_t *find_node_with_prefix(list_t *head, char *prefix, char c);
+ssize_t get_node_index(list_t *head, list_t *node);
+
+/* io_utils.c */
+void _print_string_stderr(char *string);
+int _put_char_stderr(char c);
+int _put_char_fd(char c, int fd);
+int _print_string_fd(char *string, int fd);
+
+/* input_handling.c */
+ssize_t input_buf(info_t *info, char **buf, size_t *len);
+ssize_t get_input(info_t *info);
+ssize_t read_buf(info_t *info, char *buf, size_t *i);
+int _getline(info_t *info, char **ptr, size_t *length);
+void sigintHandler(__attribute__((unused))int sig_num);
+
+/* info_manager.c */
+void init_info(info_t *info);
+void populate_info(info_t *info, char **arguments);
+void clear_info(info_t *info, int clear_all);
+
+/* info_management.c */
+void initialize_info(info_t *info);
+void populate_info(info_t *info, char **av);
+void deallocate_info(info_t *info, int all);
+
+/*error_utils.c*/
+int _str_to_int(char *str);
+void print_error_msg(info_t *info, char *error_msg);
+int print_decimal(int number, int fd);
+char *convert_to_string(long int num, int base, int flags);
+void remove_comment(char *buf);
+
+/* environment_utils.c */
+int _myenv(info_t *info);
+char *_getenv(info_t *info, const char *name);
+int _mysetenv(info_t *info);
+int _myunsetenv(info_t *info);
+int populate_env_list(info_t *info);
+
+/* environment.c */
+char **fetch_environment(info_t *info);
+int _delete_env_var(info_t *info, char *var_name);
+int _set_env_var(info_t *info, char *var_name, char *value);
+
+/* command_search.c */
+int is_cmd(info_t *info, char *path);
+char *dup_chars(char *pathstr, int start, int stop);
+char *find_path(info_t *info, char *pathstr, char *cmd);
+
+/* command_processing.c */
+int is_delimiter(info_t *info, char *buf, size_t *pos);
+void checkConti(info_t *info, char *buf, size_t *pos, size_t start, size_t len);
+int replace_aliases(info_t *info);
+int replace_variables(info_t *info);
+int replace_string(char **old, char *new);
 
 
 #endif
